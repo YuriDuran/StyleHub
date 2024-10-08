6d115afed456894a3f2cb154f45fee09998bc505
Claudio <cla.opazo@duocuc.cl>
2024-10-08 16:52:44 -0300

    si

--- Programa/Outfit/index.php
+++ Programa/Outfit/index.php
@@ -24,7 +24,7 @@ require_once 'function.php'
                 <a href="render.php">render</a>
             </div>
             <div class="col-md-4 text-center">
-                <a href="catalogo.php">catalogo</a>
+                <a href="">Proximamente..</a>
             </div>
 
             <!-- Separador de pagina -->
