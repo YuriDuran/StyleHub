8a413858e1619fcb632c41e4022c2049d53b474e
Claudio <cla.opazo@duocuc.cl>
2024-10-22 01:16:57 -0300

    FUNCIONA

--- Programa/Outfit/cargar_producto.php
+++ Programa/Outfit/cargar_producto.php
@@ -2,29 +2,6 @@
 require_once 'function.php';
 require_once 'conexion.php';
 
-// Procesar el formulario cuando se envía
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST['nombre'];
-    $descripcion = $_POST['descripcion'];
-    $precio = $_POST['precio'];
-    $categoria = $_POST['categoria'];
-    
-    $color = $_POST['color'];
-    
-    $imagenes = [
-        'frontal' => $_FILES['imagenF'],
-        'derecha' => $_FILES['imagenD'],
-        'izquierda' => $_FILES['imagenI'],
-        'trasera' => $_FILES['imagenT']
-    ];
-
-    if (guardarProducto($conexion, $nombre, $categoria, $precio, $color, $descripcion, $imagenes)) {
-        echo "<script>alert('Producto guardado con éxito');</script>";
-    } else {
-        echo "<script>alert('Error al guardar el producto');</script>";
-    }
-}
-
 // Consulta para obtener las categorías
 $query = "SELECT id_categoria, nombre_categoria FROM categorias";
 $resultado = $conexion->query($query);
@@ -48,12 +25,12 @@ $resultado = $conexion->query($query);
             <!-- Cuerpo Formulario -->
             <div class="col-md-8 fondoF">
                 <h3 class="text-center">Creacion de producto</h3>
-                <form action="">
+                <form action="logica/cargar_product.php" method="POST" enctype="multipart/form-data">
                     <div class="row">
                         <!-- Nombre del prodcuto -->
                         <div class="col-md-6">
-                            <label for="name" class="form-label">Nombre del producto</label>
-                            <input type="text" class="form-control" placeholder="Ej: Polera estampada" required>
+                            <label for="nombre" class="form-label">Nombre del producto</label>
+                            <input type="text" id="nombre" name="nombre" class="form-control" required>
                         </div>
                         <!-- Categoria del producto -->
                         <div class="col-md-6">
@@ -62,7 +39,7 @@ $resultado = $conexion->query($query);
                                 <option value="">Elegir categoría...</option>
                                 <?php
                                 // Llenar el select con las categorías
-                                while ($fila = mysqli_fetch_assoc($resultado)) {
+                                while ($fila = $resultado->fetch_assoc()) {
                                     echo "<option value='" . $fila['id_categoria'] . "'>" . $fila['nombre_categoria'] . "</option>";
                                 }
                                 ?>
@@ -71,50 +48,64 @@ $resultado = $conexion->query($query);
                                 ?>
                             </select>
                         </div>
+                        <!-- Precio del producto -->
                         <div class="col-md-6">
                             <label for="precio" class="form-label">Precio del producto</label>
-                            <input type="number" class="form-control" placeholder="Ej: 2990" required>
+                            <input type="number" id="precio" name="precio" class="form-control" step="0.01" required>
                         </div>
+                        <!-- Color del producto -->
                         <div class="col-md-6">
                             <label for="color" class="form-label">Color del producto</label>
                             <select name="color" id="color" class="form-select" required>
                                 <option value="">Elegir color...</option>
-                                <option value="1">Blanco</option>
-                                <option value="2">Negro</option>
-                                <option value="3">Rojo</option>
-                                <option value="4">Amarillo</option>
-                                <option value="5">Azul</option>
-                                <option value="6">Verde</option>
-                                <option value="7">Naranjo</option>
-                                <option value="8">Morado</option>
+                                <option value="Blanco">Blanco</option>
+                                <option value="Negro">Negro</option>
+                                <option value="Rojo">Rojo</option>
+                                <option value="Amarillo">Amarillo</option>
+                                <option value="Azul">Azul</option>
+                                <option value="Verde">Verde</option>
+                                <option value="Naranjo">Naranjo</option>
+                                <option value="Morado">Morado</option>
                             </select>
                         </div>
+                        <div class="col-md-6">
+                            <label for="stock" class="form-label">stock del producto</label>
+                            <input type="number" id="stock" name="stock" class="form-control" required>
+                        </div>
+
+                        <div class="col-md-6">
+                            <label for="talla" class="form-label">Talla del producto</label>
+                            <input type="text" id="talla" name="talla" class="form-control" required>
+                        </div>
+                        <!-- Descripcion del producto -->
                         <div class="col-md-12">
                             <label for="descripcion" class="form-label">Descripcion</label>
                             <textarea name="descripcion" id="descripcion" placeholder="Ingrese una breve descripcion del producto" class="form-control" required></textarea>
                         </div>
+                        <!-- Informacion del formulario -->
                         <div class="col-md-12 mt-4">
                             <p>En este apartado se le solicita subir 4 fotos de la prenda a publicar en nuestra pagina. Esto para poder nosotros procesar esta prenda para el mdoelado por eso se solicitan imagenes frontales, costados y trasero</p>
                         </div>
+                        <!-- Carga de todas las fotos del producto -->
                         <div class="col-md-12">
                             <label class="form-label" for="imagenF">Cargar foto frontal</label>
-                            <input type="file" class="form-control" id="foto" name="foto">
+                            <input type="file" class="form-control" id="imagenF" name="imagenF" required>
                         </div>
                         <div class="col-md-12">
                             <label class="form-label" for="imagenD">Cargar foto lado derecho</label>
-                            <input type="file" class="form-control" id="foto" name="foto">
+                            <input type="file" class="form-control" id="imagenD" name="imagenD" required>
                         </div>
                         <div class="col-md-12">
                             <label class="form-label" for="imagenI">Cargar foto lado izquierdo</label>
-                            <input type="file" class="form-control" id="foto" name="foto">
+                            <input type="file" class="form-control" id="imagenI" name="imagenI" required>
                         </div>
                         <div class="col-md-12">
                             <label class="form-label" for="imagenT">Cargar foto trasero</label>
-                            <input type="file" class="form-control" id="foto" name="foto">
+                            <input type="file" class="form-control" id="imagenT" name="imagenT" required>
                         </div>
 
                         <div class="col-md-12 text-center Cboton">
-                            <button class="btn enviar">Registrar producto</button>
+                            <button type="submit" class="btn enviar">Registrar producto</button>
                         </div>
                     </div>
 
@@ -134,3 +125,4 @@ $resultado = $conexion->query($query);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
+
